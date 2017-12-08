cbbca5e5cc1a83ee132460737923b04f99dad06d
Emre Altunbilek <emrealtunbilek@gmail.com>
2017-12-09 02:22:18 +0300

    onSaveInstanceState yokken

---
+++ app/src/main/java/com/emrealtunbilek/activitylifecycle/ActivityB.kt
@@ -0,0 +1,22 @@
+package com.emrealtunbilek.activitylifecycle
+
+import android.os.Bundle
+import android.os.PersistableBundle
+import android.support.v7.app.AppCompatActivity
+import android.util.Log
+import kotlinx.android.synthetic.main.activity_main.*
+
+/**
+ * Created by Emre on 9.12.2017.
+ */
+class ActivityB : AppCompatActivity() {
+
+    val TAG = "EMRE"
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_b)
+        Log.d(TAG, "onCreate:")
+
+    }
+}
---
+++ app/src/main/java/com/emrealtunbilek/activitylifecycle/ActivityC.kt
@@ -0,0 +1,15 @@
+package com.emrealtunbilek.activitylifecycle
+
+import android.os.Bundle
+import android.support.v7.app.AppCompatActivity
+
+/**
+ * Created by Emre on 9.12.2017.
+ */
+class ActivityC : AppCompatActivity() {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_c)
+    }
+}
